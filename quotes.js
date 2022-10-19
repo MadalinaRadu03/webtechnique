40e150e7915a0b95c32ea291f78d7ed570535b80
MadalinaRadu03 <113447997+MadalinaRadu03@users.noreply.github.com>
2022-10-19 13:50:55 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -4,5 +4,12 @@
   "text":"A writer never has a vacation. For a writer life consists of either writing or thinking about writing." ,
 
 "author" : "Eugene Ionesco "
+ },
+ {
+ "text":
+"A story is a letter that the author writes to himself, to tell himself things that he would be unable to discover otherwise."
+
+"author":
+"Carlos Ruiz Zafón"
  }
 ]
