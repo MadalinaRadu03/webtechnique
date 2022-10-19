163e51bc60b26d39f91fb79b5d3e7919852fb938
MadalinaRadu03 <113447997+MadalinaRadu03@users.noreply.github.com>
2022-10-19 13:51:45 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -6,7 +6,7 @@
 "author" : "Eugene Ionesco "
  },
  {
- "text":"A story is a letter that the author writes to himself, to tell himself things that he would be unable to discover otherwise."
+ "text":"A story is a letter that the author writes to himself, to tell himself things that he would be unable to discover otherwise.",
 
 "author":"Carlos Ruiz Zafón"
  }
